1a6e224c0014a3f7b510e54e71c8582532d56133
Ammar <s95ammar@gmail.com>
2020-05-14 18:28:21 +0300

    Prepared ServiceLocator for TaskDetailFragmentTest

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
@@ -4,8 +4,14 @@ import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.example.android.architecture.blueprints.todoapp.R
+import com.example.android.architecture.blueprints.todoapp.ServiceLocator
 import com.example.android.architecture.blueprints.todoapp.data.Task
-import org.junit.Assert.*
+import com.example.android.architecture.blueprints.todoapp.data.source.FakeTestRepository
+import com.example.android.architecture.blueprints.todoapp.data.source.IDefaultTasksRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -16,14 +22,31 @@ import org.junit.runner.RunWith
 * These annotations help us group and choose which size of tests to run when running tests
 */
 @MediumTest
+@ExperimentalCoroutinesApi
 class TaskDetailFragmentTest {
 
+    private lateinit var repository: IDefaultTasksRepository
+
+    @Before
+    fun initRepository() {
+        repository = FakeTestRepository()
+        ServiceLocator.tasksRepository = repository
+    }
+
+    @After
+    fun cleanupDb() = runBlockingTest {
+        ServiceLocator.resetRepository()
+    }
+
     @Test
-    fun activeDetails_DisplayedInUi() {
+    fun activeDetails_DisplayedInUi() = runBlockingTest {
         val activeTask = Task("Active task", "Bla", false)
+
+        repository.saveTask(activeTask)
 
         val bundle = TaskDetailFragmentArgs(activeTask.id).toBundle()
         launchFragmentInContainer<TaskDetailFragment>(bundle, R.style.AppTheme)
+        Thread.sleep(2000)
 
 
     }
